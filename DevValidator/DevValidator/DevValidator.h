9c9e00480117e799f08a7a3d8804a17767e023d7
Rajeshgandru <rajesh@thorsignnia.net>
2021-03-22 16:41:26 +0530

    first commit

---
+++ DevValidator/DevValidator/DevValidator.h
@@ -0,0 +1,18 @@
+//
+//  DevValidator.h
+//  DevValidator
+//
+//  Created by admin on 3/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DevValidator.
+FOUNDATION_EXPORT double DevValidatorVersionNumber;
+
+//! Project version string for DevValidator.
+FOUNDATION_EXPORT const unsigned char DevValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevValidator/PublicHeader.h>
+
+
